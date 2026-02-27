c9cfb7813d9fd6fdae15a9ef14587abf283a8b2e
Miguel Cortez <mickeycortez2002@gmail.com>
2026-02-27 17:16:34 -0600

    feat: Se agrego el archivo para el footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-4 text-center">
+            <p>&copy; {new Date().getFullYear()} Mi Portafolio. Todos los derechos reservados.</p>
+        </footer>
+    );
+}
+
+export default Footer;
